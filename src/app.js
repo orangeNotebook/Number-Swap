6386a5fed3889c82dfd1a79f06df6f1a4f81a758
Kacper Kiermasz <6529076+orangeNotebook@users.noreply.github.com>
2020-08-25 10:47:23 +0100

    Fixed swapping

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const readline = require("readline");
 let rl = readline.createInterface(process.stdin, process.stdout);
 
 function inputHandler(before, answer, list) {
-  list[list.indexOf(Number(before))] = Number(answer);
+  var firstIndex = list.indexOf(Number(before));
+  var secondIndex = list.indexOf(Number(answer));
+  list[firstIndex] = Number(answer);
+  list[secondIndex] = Number(before);
   getInput(list);
 }
 
@@ -12,10 +15,18 @@ function getInput(list) {
   rl.question("Number to swap or Q to exit: ", (before) => {
     if (before == "Q") {
       rl.close();
+    } else if (before >= 16) {
+      console.log(`${before} does not exist in the list`);
+      getInput(list);
     } else {
-      rl.question(`Swap ${before} with: `, (answer) =>
-        inputHandler(before, answer, list)
-      );
+      rl.question(`Swap ${before} with: `, (answer) => {
+        if (answer >= 16) {
+          console.log(`${answer} does not exist in the list`);
+          getInput(list);
+        } else {
+          inputHandler(before, answer, list);
+        }
+      });
     }
   });
 }
