10fe6aaff27b0797d6a06ac6ed1e102ca2921e71
Kacper Kiermasz <6529076+orangeNotebook@users.noreply.github.com>
2020-09-27 23:22:07 +0100

    variable name changes

--- src/app.js
+++ src/app.js
@@ -2,29 +2,29 @@ const readline = require("readline");
 
 let rl = readline.createInterface(process.stdin, process.stdout);
 
-function inputHandler(before, answer, list) {
-  var firstIndex = list[Number(before) - 1];
-  var secondIndex = list[Number(answer) - 1];
-  list[Number(before) - 1] = secondIndex;
-  list[Number(answer) - 1] = firstIndex;
+function inputHandler(n1, n2, list) {
+  var firstIndex = list[n1 - 1];
+  var secondIndex = list[n2 - 1];
+  list[n1 - 1] = secondIndex;
+  list[n2 - 1] = firstIndex;
   getInput(list);
 }
 
 function getInput(list) {
   console.log(list);
-  rl.question("Number to swap or Q to exit: ", (before) => {
-    if (before == "Q") {
+  rl.question("Number to swap or Q to exit: ", (swap1) => {
+    if (swap1 == "Q") {
       rl.close();
-    } else if (before >= 16) {
-      console.log(`${before} does not exist in the list`);
+    } else if (swap1 >= 16) {
+      console.log(`${swap1} does not exist in the list`);
       getInput(list);
     } else {
-      rl.question(`Swap ${before} with: `, (answer) => {
-        if (answer >= 16) {
-          console.log(`${answer} does not exist in the list`);
+      rl.question(`Swap ${swap1} with: `, (swap2) => {
+        if (swap2 >= 16) {
+          console.log(`${swap2} does not exist in the list`);
           getInput(list);
         } else {
-          inputHandler(before, answer, list);
+          inputHandler(Number(swap1), Number(swap2), list);
         }
       });
     }
